6aa55041ac5dcd32998faffcb852bda315f35751
Cedrico <118186038+Cedricoooo@users.noreply.github.com>
2026-07-22 15:38:09 +0200

    Create rustic navbar

---
+++ src/Components/Layouts/Navbar.tsx
@@ -0,0 +1,29 @@
+﻿import {NavLink} from 'react-router-dom';
+import './Navbar.css';
+
+function NavBar() {
+    return (
+        <header className="navbar">
+            <h1 className="title">Black Hole Odyssey</h1>
+            <nav className="nav-links">
+                <NavLink to="/" end>
+                    Home
+                </NavLink>
+                <NavLink to="/games">
+                    Games
+                </NavLink>
+                <NavLink to="/packages">
+                    Packages
+                </NavLink>
+                <NavLink to="/about">
+                    About
+                </NavLink>
+                <NavLink to="/contact">
+                    Contact
+                </NavLink>
+            </nav>
+        </header>
+    );
+}
+
+export default NavBar;
