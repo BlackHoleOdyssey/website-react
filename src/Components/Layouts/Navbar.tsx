4695a4e0edc4e4e2aa2a1f5ded10804a4e20bd35
Cedrico <118186038+Cedricoooo@users.noreply.github.com>
2026-07-22 16:38:00 +0200

    Import neonbladeUI & update navbar visual

--- src/Components/Layouts/Navbar.tsx
+++ src/Components/Layouts/Navbar.tsx
@@ -1,10 +1,35 @@
-﻿import {NavLink} from 'react-router-dom';
+﻿import {useState, useEffect} from 'react';
+import {NavLink} from 'react-router-dom';
 import './Navbar.css';
+import NeonGlow from "../neonblade-ui/neon-glow";
+
 
 function NavBar() {
+    const [isScrolled, setIsScrolled] = useState(false);
+    
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 50);
+        };
+        
+        window.addEventListener('scroll', handleScroll);
+        
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <header className="navbar">
-            <h1 className="title">Black Hole Odyssey</h1>
+        <header className={isScrolled ? 'navbar navbar-scrolled' : 'navbar'}>
+            <NeonGlow
+            colors="#ffffff"
+            glowIntensity={"subtle"}
+            className="title"
+            >
+            <h1>
+                Black Hole Odyssey
+            </h1>
+            </NeonGlow>
             <nav className="nav-links">
                 <NavLink to="/" end>
                     Home
