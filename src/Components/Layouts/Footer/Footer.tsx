fc96e644c336816d8acd18eb9605e59f15e43d5f
Cedrico <118186038+Cedricoooo@users.noreply.github.com>
2026-07-23 13:33:22 +0200

    Add link to the social icon

--- src/Components/Layouts/Footer/Footer.tsx
+++ src/Components/Layouts/Footer/Footer.tsx
@@ -16,26 +16,30 @@ function Footer() {
                             An independent studio that creates games and packages for Unity.
                         </p>
                     </div>
-                    
+
                     <div>
                         <p className={styles.copyright}>© 2026 Black Hole Odyssey. All rights reserved.</p>
                     </div>
-                    
+
                 </div>
 
                 <div className={styles.footerNav}>
                     <div className={styles.footerExplorer}>
                         <p className={styles.footerHeading}>Explorer</p>
-                        <NavLink to="/games" className={({ isActive }) => isActive ? `${commonStyles.navLink} ${commonStyles.navLinkActive}` : commonStyles.navLink}>
+                        <NavLink to="/games"
+                                 className={({isActive}) => isActive ? `${commonStyles.navLink} ${commonStyles.navLinkActive}` : commonStyles.navLink}>
                             Games
                         </NavLink>
-                        <NavLink to="/packages" className={({ isActive }) => isActive ? `${commonStyles.navLink} ${commonStyles.navLinkActive}` : commonStyles.navLink}>
+                        <NavLink to="/packages"
+                                 className={({isActive}) => isActive ? `${commonStyles.navLink} ${commonStyles.navLinkActive}` : commonStyles.navLink}>
                             Packages
                         </NavLink>
-                        <NavLink to="/about" className={({ isActive }) => isActive ? `${commonStyles.navLink} ${commonStyles.navLinkActive}` : commonStyles.navLink}>
+                        <NavLink to="/about"
+                                 className={({isActive}) => isActive ? `${commonStyles.navLink} ${commonStyles.navLinkActive}` : commonStyles.navLink}>
                             About
                         </NavLink>
-                        <NavLink to="/contact" className={({ isActive }) => isActive ? `${commonStyles.navLink} ${commonStyles.navLinkActive}` : commonStyles.navLink}>
+                        <NavLink to="/contact"
+                                 className={({isActive}) => isActive ? `${commonStyles.navLink} ${commonStyles.navLinkActive}` : commonStyles.navLink}>
                             Contact
                         </NavLink>
                     </div>
@@ -43,10 +47,35 @@ function Footer() {
 
                 <div className={styles.footerSocial}>
                     <p className={styles.footerHeading}>Social</p>
-                    <img src={`${import.meta.env.BASE_URL}icons/linkedin-in-white.png`} alt="LinkedIn" className={styles.socialIcon} />
-                    <img src={`${import.meta.env.BASE_URL}icons/itchio-white.svg`} alt="itch.io" className={styles.socialIcon} />
-                    <img src={`${import.meta.env.BASE_URL}icons/github-white.svg`} alt="GitHub" className={styles.socialIcon} />
-                    <img src={`${import.meta.env.BASE_URL}icons/discord-white.svg`} alt="Discord" className={styles.socialIcon} />
+                    <a href="https://www.linkedin.com/in/cedric-roux-4a171b253/" target="_blank"
+                       rel="noopener noreferrer">
+                        <img
+                            className={styles.socialIcon}
+                            src={`${import.meta.env.BASE_URL}icons/linkedin-in-white.png`}
+                            alt="LinkedIn"
+                        />
+                    </a>
+                    <a href="https://cedricoo.itch.io/" target="_blank" rel="noopener noreferrer">
+                        <img
+                            className={styles.socialIcon}
+                            src={`${import.meta.env.BASE_URL}icons/itchio-white.svg`}
+                            alt="itch.io"
+                        />
+                    </a>
+                    <a href="https://github.com/Cedricoooo" target="_blank" rel="noopener noreferrer">
+                        <img
+                            className={styles.socialIcon}
+                            src={`${import.meta.env.BASE_URL}icons/github-white.svg`}
+                            alt="GitHub"
+                        />
+                    </a>
+                    <a href="https://discord.gg/6CYUMAx65X" target="_blank" rel="noopener noreferrer">
+                        <img
+                            className={styles.socialIcon}
+                            src={`${import.meta.env.BASE_URL}icons/discord-white.svg`}
+                            alt="Discord"
+                        />
+                    </a>
                 </div>
             </section>
         </footer>
