ef58056a1a984a3b7c4e24040f3946d432af27ca
Cedrico <118186038+Cedricoooo@users.noreply.github.com>
2026-07-23 19:46:04 +0200

    Fix buton draw over the nav bar

--- src/Pages/Home/home.tsx
+++ src/Pages/Home/home.tsx
@@ -6,28 +6,33 @@ import NeonGlow from "../../Components/neonblade-ui/neon-glow";
 function Home() {
     
     return (
-        <div className={style.hero}>
-            <NeonGlow
-                colors="#ffffff"
-                glowIntensity={"subtle"}
-                className={styles.title}
-            >
-                <h1 className={style.title}>
-                    Black Hole Odyssey
-                </h1>
-            </NeonGlow>
-            <p className={style.description}>
-                Independent Studio - Games and Packages on Unity
-            </p>
-            <NavLink to="/games" className={style.viewGames}>
-                <span className={style.buttonText}>View Games</span>
-                <div className={style.glowWrapper}>
-                    <NeonGlow colors="#ffffff" glowIntensity="subtle">
-                        View Games
-                    </NeonGlow>
-                </div>
-            </NavLink>
-        </div>
+        <section>
+            <section className={style.hero}>
+                <NeonGlow
+                    colors="#ffffff"
+                    glowIntensity={"subtle"}
+                    className={styles.title}
+                >
+                    <h1 className={style.title}>
+                        Black Hole Odyssey
+                    </h1>
+                </NeonGlow>
+                <p className={style.description}>
+                    Independent Studio - Games and Packages on Unity
+                </p>
+                <NavLink to="/games" className={style.viewGames}>
+                    <span className={style.buttonText}>View Games</span>
+                    <div className={style.glowWrapper}>
+                        <NeonGlow colors="#ffffff" glowIntensity="subtle">
+                            View Games
+                        </NeonGlow>
+                    </div>
+                </NavLink>
+            </section>
+            <section>
+                
+            </section>
+        </section>
     );
 }
 
